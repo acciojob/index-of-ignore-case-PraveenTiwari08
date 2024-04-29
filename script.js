0f1af6be5cced71a5782eaeeec9bbe153fd8d942
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-04-29 23:26:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function indexOfIgnoreCase(s1, s2) {
 	if(!s2.includes(s1)){
 		return -1;
 	}else{
-		for(let i = 0 ; i < s1.length ; i++){
-			 s2;
+		for(let index in s2){
+			return index;
 		}
 	}
 }
 
 // Please do not change the code below
-// const s1 = prompt("Enter s1:");
-// const s2 = prompt("Enter s2:");
-// alert(indexOfIgnoreCase(s1, s2));
+const s1 = prompt("Enter s1:");
+const s2 = prompt("Enter s2:");
+alert(indexOfIgnoreCase(s1, s2));
